46bbe82e250f0f604c09bae21460388f2c001b1f
Spencer Schurk <spencer@joyrun.com>
2019-02-06 14:00:55 -0800

    Ground scrolls

---
+++ Lab 5/Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour {
+
+    private Renderer rend;
+    private float horizontalLength;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        horizontalLength = rend.bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (transform.position.x < -horizontalLength) {
+            RepositionBackground();
+        }
+    }
+
+    private void RepositionBackground() {
+        Vector2 groundOffset = new Vector2(horizontalLength * 2f, 0);
+        transform.position = (Vector2) transform.position + groundOffset;
+    }
+}
---
+++ Lab 5/Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour {
+    
+    public float speed;
+    private Rigidbody2D rb2d;
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(speed, 0);
+    }
+    void Update()
+    {
+        
+    }
+}
